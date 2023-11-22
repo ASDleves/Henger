3684d3c16abd4263370c667694b501dbf8f2f5c2
ASDleves <ricsi20020302@gmail.com>
2023-11-22 13:41:13 +0100

    uml alap

--- src/main/HengerProgram.java
+++ src/main/HengerProgram.java
@@ -1,9 +1,34 @@
 
 package main;
 
+import java.util.List;
+private Henger[] hengerek;
 public class HengerProgram {
 
     public static void main(String[] args) {
+        new HengerProgram();
+    }
+
+    public HengerProgram(){
+        
+    }
+    
+    public double atlagTerfogat(){
+        
+        return 0;
+        
+    }
+    public double csovekSulya(){
+        
+        return 0;
+        
+    }
+    private List lista(){
+        
+        return null;
+        
+    }
+    public void run(){
         
     }
     
